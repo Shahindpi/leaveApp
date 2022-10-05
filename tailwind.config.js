5bb28489b2ecc1382b6e15b911b3179dd6da5194
Shahindpi <105318666+CitrusAd-SEBPO@users.noreply.github.com>
2022-10-05 13:20:53 +0600

    Login page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx}", "public/index.html"],
   theme: {
     screens: {
+      xs: '300px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
